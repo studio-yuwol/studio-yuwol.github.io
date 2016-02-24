638454f8ad2c260b7de54c28d5e8a28299d51df5
arisnoba-test <arisnoba@me.com>
2016-02-25 02:47:55 +0900

    sdffds

---
+++ lab/js/lnb.js
@@ -0,0 +1,8 @@
+/*
+  SlidesJS 3.0.4 http://slidesjs.com
+  (c) 2013 by Nathan Searles http://nathansearles.com
+  Updated: June 26th, 2013
+  Apache License: http://www.apache.org/licenses/LICENSE-2.0
+*/
+
+
